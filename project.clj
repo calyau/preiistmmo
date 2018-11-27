6c400195b747e3bb3e8993f280ab0b67060b00db
Duncan McGreggor <oubiwann@gmail.com>
2018-11-27 15:56:20 -0600

    Added initial project files.

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject preiistmmo "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/calyau/preiistmmo"
+  :license {
+    :name "Apache License, Version 2.0"
+    :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [org.clojure/clojure "1.10.0-RC2"]]
+  :plugins [
+    [lein-ltest "0.3.0"]]
+  :repl-options {
+    :init-ns preiistmmo.core}
+  :main preiistmmo.core
+  :aot [preiistmmo.core])
